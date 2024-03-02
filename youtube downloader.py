a42ef2c218ab4ed945b0cc885dc85c2b857fefa2
dinobeacon <160197951+dinobeacon@users.noreply.github.com>
2024-03-02 14:26:13 -0800

    Add files via upload
    This update shows you the available links to download from and allows you to choose which one to download.
    Ensure that you only enter the positions shown or it will return an error.

---
+++ youtube downloader.py
@@ -0,0 +1,61 @@
+from firefox_profile import FirefoxProfile
+from pytube import YouTube
+
+Dlink = []
+Dlink_Name = []
+
+def youtube_downloader(link):
+    yt = YouTube(link)
+    yd = yt.streams.get_highest_resolution()
+    yd.download('C:\\Users\\Home\\Videos\\ytd')
+
+
+for profile in FirefoxProfile.get_profiles():
+    recovery_data = profile.get_recovery_data()
+    if recovery_data is None:
+        continue
+    for i, window in enumerate(recovery_data.windows):
+        print(f"window {i}")
+        print(f"  tabs:")
+        for j, tab in enumerate(window.tabs):
+            print(f"    tab {j}")
+            print(f"      url: {tab.url}")
+            if 'https://www.youtube.com/' == tab.url:
+                    print("unable to use general directory for youtube")
+            elif 'youtube.com' in tab.url:
+                Dlink.append(tab.url)
+
+
+for z in range(len(Dlink)):
+    urlz = YouTube(Dlink[z])
+    url_title = urlz.title
+    Dlink_Name.append("Position (" + str(z) + ") :  " + url_title)
+
+print(" ")
+print(" These are the retrieved links from youtube")
+print (" ")
+print( '\n'.join(Dlink))
+print(" ")
+print("These are the names of the videos retrieved ")
+print(" ")
+print( '\n'.join(Dlink_Name))
+print(" ")
+
+
+Newlink_positions = []
+
+
+for l in range(len(Dlink)):
+    a = input("Please enter which video to download. Please only use positions from previois printed list ")
+    if a == "end":
+        break
+    elif a != "end":
+       a = int(a)
+       Newlink_positions.append(a)
+
+
+
+for u in range(len(Newlink_positions)):
+    print("position : " + str(Newlink_positions[u]))
+    youtube_downloader(Dlink[Newlink_positions[u]])
+    print("download successful")
